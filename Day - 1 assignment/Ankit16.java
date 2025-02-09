db1c8c35ece3106ed742efccfd62a0362e5e5484
Ankit Jindal <jindal8901@gmail.com>
2025-02-09 12:53:17 +0530

    Add files via upload

---
+++ Day - 1 assignment/Ankit16.java
@@ -0,0 +1,13 @@
+ // write a program which prints  the following information about at least 5 persons . 
+
+public class Ankit16 {
+    public static void main(String[] args) {
+    System.out.println("NAME       MAIL_ID                  EMPLOYEE-CODE        PHONE_NUMBER");
+    System.out.println("ANKIT      jindal8901@gmail.com     Emp-091                9985413431");
+    System.out.println("Chetan     chetan8902@gmail.com     Emp-092                9497644546");
+    System.out.println("Harshit    harsita8903@gmail.com    Emp-093                9151243437");
+    System.out.println("kirti      kirti8904@gmail.com      Emp-094                8942134364");
+    System.out.println("Himanshu   himanshu@gmail.com       Emp-095                9944643458");
+    
+}    
+}
---
+++ Day - 1 assignment/Program2.java
@@ -0,0 +1,10 @@
+
+// Write a program to print the following line on the screen along with the quotes:
+
+public class Program2 {
+
+    public static void main(String[] args) {
+        System.out.println("\" can we print'\\' with system.out.println() statement?\"");  
+}
+}  
+
