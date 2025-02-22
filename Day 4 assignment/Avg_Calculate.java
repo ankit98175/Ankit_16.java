b42d7985aa45d74629132e76cf540bb52edee917
Ankit Jindal <jindal8901@gmail.com>
2025-02-22 10:30:46 +0530

    Add files via upload

---
+++ Day 4 assignment/Avg_Calculate.java
@@ -0,0 +1,13 @@
+public class Avg_Calculate {
+    
+    public static void main(String[] args) {
+        int april=12;
+        int may=14;
+        int june=8;
+        double avg=(april+may+june)/3.0;
+        System.out.println("Synsets Entered for March: "+april);
+        System.out.println("Synsets Entered for May: "+may);
+        System.out.println("Synsets Entered for June: "+june);
+        System.out.println("Synsets Entered for Average: "+avg);
+    }
+}
